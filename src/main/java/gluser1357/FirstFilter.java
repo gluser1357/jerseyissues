893155a636b0040c89f8007730f1b576512e082f
sre <s.reifegerste@mapchart.com>
2023-09-07 10:56:34 +0200

    added test code to create exception on getReader()

--- src/main/java/gluser1357/FirstFilter.java
+++ src/main/java/gluser1357/FirstFilter.java
@@ -2,6 +2,9 @@ package gluser1357;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
@@ -11,12 +14,25 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FirstFilter
 extends HttpFilter {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FirstFilter.class);
+
 	 @Override
 	 public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 	 throws IOException, ServletException {
 
 		 String query = request.getQueryString();
-		 if (query != null && query.contains("addRowWith")) request.getInputStream();
+		 if (query != null && query.contains("addRowWith")) {
+			 request.getInputStream();
+			 
+			 /*
+			 try {
+				 request.getReader();
+			 }
+			 catch (Exception e) {
+				 LOG.error("getReader(): ", e);
+			 }
+			 */
+		 }
 		 chain.doFilter(request, response);
 	 }
 }
